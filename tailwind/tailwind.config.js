defd0813e13ef90f82aa30b6b5032565e07e83e6
Farzan Kazi <65282015+kazi-farzan@users.noreply.github.com>
2025-11-22 12:45:12 +0530

    Add files via upload

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    '../index.html',
+    '../css/**/*.css',
+    '../script.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
